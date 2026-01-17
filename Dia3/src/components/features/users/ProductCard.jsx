b411bbcfb893eb235c02e806a0ffc727b483c217
Zenen Contreras Royero <zenencontreras1@gmail.com>
2026-01-17 14:43:29 -0500

    fix: accesibility 82 to 100

--- Dia3/src/components/features/users/ProductCard.jsx
+++ Dia3/src/components/features/users/ProductCard.jsx
@@ -3,12 +3,12 @@
     <div className="group bg-white rounded-xl shadow-sm hover:shadow-xl transition-all duration-300 border border-gray-100 overflow-hidden">
         <div className="flex flex-col p-6 ">
             <div className="px-3 py-1 bg-blue-50 rounded-2xl mb-4">
-                <h5 className="text-blue-400 text-sm font-bold">ID: {user.id}</h5>
+                <h2 className="text-blue-600 text-sm font-bold">ID: {user.id}</h2>
             </div>
-            <h1 className="font-bold text-xl text-blue-950 mb-1">{user.name}</h1>
-            <h4 className="text-gray-400 mb-4">@{user.username}</h4>
-            <h3 className="text-gray-400">📧 {user.email}</h3>
-            <h3 className="text-gray-400 mb-4">🌐 {user.website}</h3>
+            <h2 className="font-bold text-xl text-blue-950 mb-1">{user.name}</h2>
+            <h3 className="text-gray-500 mb-4">@{user.username}</h3>
+            <h4 className="text-gray-500">📧 {user.email}</h4>
+            <h4 className="text-gray-500 mb-4">🌐 {user.website}</h4>
             <button className="bg-blue-800 px-3 py-2 rounded-lg hover:cursor-pointer active:scale-95 duration-200"> Ver Perfil Completo</button>
         </div>
     </div>
